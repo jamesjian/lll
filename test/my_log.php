bd855964fec57460ef6bb524cfeb17006de724be
jamesjian <james.jian.hang@gmail.com>
2012-11-27 21:37:09 +1100

    log

---
+++ test/my_log.php
@@ -0,0 +1,12 @@
+$_SESSION:Array
+(
+    [success_message] => 
+    [error_message] => 
+    [user] => Array
+        (
+            [user_id] => 1
+            [user_name] => aaa
+        )
+
+)
+<span style="color:red;">file:E:\wamp54\www\lll\application\transaction\user.phpline:332class:App\Transaction\Usermethod:App\Transaction\User::verify_user</span><br /><br />
